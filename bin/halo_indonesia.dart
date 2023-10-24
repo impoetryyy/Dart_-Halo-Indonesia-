475e0f602d3b04ba46eaea451d3b84d3379cbb12
Putri <96984201+impoetryyy@users.noreply.github.com>
2023-10-24 17:09:52 +0700

    Add files via upload

---
+++ bin/halo_indonesia.dart
@@ -0,0 +1,7 @@
+import 'package:halo_indonesia/halo_indonesia.dart' as halo_indonesia;
+
+void main(List<String> arguments) {
+  print('Halo Indonesia');
+  print('Selamat Pagi Indonesia');
+  print('Jaya Indonesia');
+// }
